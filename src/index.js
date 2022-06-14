ebe94d6783c19621c57ee504ca4126b22851f997
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-06-14 11:20:38 +0100

    completed logic

--- src/index.js
+++ src/index.js
@@ -9,125 +9,145 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-// set staff array
-// this is where i am going to push all the answers from the inquirer questions
-const teamArr = [];
-
 // start sequence of questions
 // what role would the user like to select
 // push answers into array
 
 // prompt the question to users
-const roleofTeam = inquirer.prompt([
-  {
-    type: "list",
-    message: "Please select a role",
-    name: "role",
-    choices: ["Engineer", "Intern", "Manager"],
-  },
-
-  // then if role of Team === Engineer render the engineer questions
-]);
-
-// make sure the user
-
-// then if role of Team === Engineer render the engineer questions
-//then if role of Team === Intern render the intern questions
-//then if role of Team === Manager render the manager questions
+const rolesOfTeam = {
+  name: "roles",
+  type: "list",
+  message: "Please select a role",
+  choices: ["Engineer", "Intern", "Manager"],
+};
 
 const engineerQuestions = [
   {
+    name: "name",
     type: "input",
     message: "Please enter employee name",
-    name: "name",
-
     // ensure there are only letters entered here
   },
   {
+    name: "id",
     type: "input",
     message: "Please enter employee id",
-    name: "id",
-
     // ensure there are only number entered here
   },
   {
+    name: "email",
     type: "input",
     message: "Please enter employee email address",
-    name: "email",
 
     // ensure there is an @.com inside this to validate
   },
   {
+    name: "githubUsername",
     type: "input",
     message: "Please enter engineer github username",
-    name: "githubUsername",
-    // after i get the answers for each question i put the answers in its own array
   },
 ];
-
 // intern questions
-
 const internQuestions = [
   {
+    name: "name",
     type: "input",
     message: "Please enter employee name",
-    name: "name",
-    // ensure there are only letters entered here
   },
   {
+    name: "id",
     type: "input",
     message: "Please enter employee id",
-    name: "id",
-
     // ensure there are only number entered here
   },
   {
+    name: "email",
     type: "input",
     message: "Please enter employee email address",
-    name: "email",
-    // ensure there is an @.com inside this to validate
   },
   {
+    name: "school",
     type: "input",
     message: "Please enter employee school",
-    name: "school",
-    // ensure only letters in this school answer
-    // after i get the answers for each question i put the answers in its own array
   },
 ];
 
 // manager questions
 const managerQuestions = [
   {
+    name: "name",
     type: "input",
     message: "Please enter employee name",
-    name: "name",
-    // ensure there are only letters entered here
   },
   {
+    name: "id",
     type: "input",
     message: "Please enter employee id",
-    name: "id",
-    // ensure there are only number entered here
   },
   {
+    name: "email",
     type: "input",
     message: "Please enter employee email address",
-    name: "email",
-    // ensure there is an @.com inside this to validates
   },
   {
+    name: "officeNumber",
     type: "input",
     message: "Please enter manager officeNumber",
-    name: "officeNumber",
-    // ensure only numbers are in the answers
   },
-
-  // after i get the answers for each question i put the answers in its own array
 ];
+
+const finalQuestion = {
+  name: "final",
+  type: "confirm",
+  message: "Would you like to add any more employees?",
+  default: "yes",
+};
+
+const init = async () => {
+  // set staff array
+  // this is where i am going to push all the answers from the inquirer questions
+  const teamArr = [];
+  // start loop
+  let roleInProgress = true;
+
+  while (roleInProgress) {
+    //display answers
+    const { roles } = await inquirer.prompt(rolesOfTeam);
+    console.log(roles);
+    //if engineer
+    if (roles === "Engineer") {
+      //prompt engineer questions and answers
+      const engineerAnswers = await inquirer.prompt(engineerQuestions);
+      console.log(engineerAnswers);
+      // create a new instance of engineer to push into teamArr
+      const engineer = new Engineer(engineerAnswers);
+      teamArr.push(engineer);
+    }
+    //if intern
+    if (roles === "Intern") {
+      //prompt engineer questions and answers
+      const internAnswers = await inquirer.prompt(internQuestions);
+      console.log(internAnswers);
+      const intern = new Engineer(internAnswers);
+      teamArr.push(intern);
+    }
+    // if manager
+    if (roles === "Manager") {
+      //prompt engineer questions and answers
+      const managerAnswers = await inquirer.prompt(managerQuestions);
+      console.log(managerAnswers);
+      const manager = new Engineer(managerAnswers);
+      teamArr.push(managerAnswers);
+    }
+    const { final } = await inquirer.prompt(finalQuestion);
+
+    if (!final) {
+      roleInProgress = false;
+    }
+  }
+  fs.writeFile;
+};
 
 // when the user finishes building there team they exit the application and there HTML is generated
 
-//
-
-// then write file to sync
+init();
